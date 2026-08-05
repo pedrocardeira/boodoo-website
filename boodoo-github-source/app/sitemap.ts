e6b878b8267b7ac3b9b28294e8fb09c527700518
pedrocardeira <pedro.cardeira@gmail.com>
2026-08-05 23:01:21 +0200

    Update sitemap.ts

--- boodoo-github-source/app/sitemap.ts
+++ boodoo-github-source/app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 import { allTrackerPages } from "../lib/tracker-pages";
-
+export const dynamic = "force-static";
 export default function sitemap(): MetadataRoute.Sitemap {
   const lastModified = new Date();
 
